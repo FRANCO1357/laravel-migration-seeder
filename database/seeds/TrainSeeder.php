4d52e8209399cc4d468af82cd1183cfaaa323513
FRANCO1357 <fmelani13@gmail.com>
2022-09-20 18:01:17 +0200

    2nd record seeder

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -14,15 +14,15 @@ class TrainSeeder extends Seeder
     public function run()
     {
         $new_train = new Train();
-        $new_train->company = 'Trenitalia';
-        $new_train->start_station = 'Milano';
-        $new_train->end_station = 'Roma';
-        $new_train->start_time = '2020/05/20';
-        $new_train->end_time = '2020/05/21';
-        $new_train->train_code = 3745;
-        $new_train->carriages_number = 25;
-        $new_train->in_time = true;
-        $new_train->delay = false;
+        $new_train->company = 'Italo';
+        $new_train->start_station = 'Bologna';
+        $new_train->end_station = 'Parma';
+        $new_train->start_time = '2020/07/20';
+        $new_train->end_time = '2020/08/21';
+        $new_train->train_code = 2673;
+        $new_train->carriages_number = 21;
+        $new_train->in_time = false;
+        $new_train->delay = true;
 
         $new_train->save();
     }
